3b8dcd5c12697c1ec90b59c7d8e808a76caf7794
Peter <sceptix@gmail.com>
2022-07-12 00:15:10 -0400

    and he finds his way...with a map TBH

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,14 @@ use maze_walker::*;
 
     const PLAYER_SPRITE: &str = "actors/runner_1_32.png";
     const PLAYER_SIZE: (f32, f32) = (32.,32.);
+    const ACTOR_SCALE: f32 = 0.5;
 
-    const MAP_SPRITE: &str = "mazes/maze(1).png";
-    const FQ_MAP_SPRITE: &str = "assets/mazes/maze(1).png";
+    const MAP_SPRITE: &str = "mazes/maze(8).png";
+    const FQ_MAP_SPRITE: &str = "assets/mazes/maze(8).png";
     const MAP_SIZE: (f32, f32) = (41.,41.);
+    const MAP_SCALE: f32 = 4.;
 
-//#End Region   --- Asset Constants
+    //#End Region   --- Asset Constants
 
 
 fn main() {
@@ -27,21 +29,23 @@ fn main() {
     let entrances = maze.find_start();
     println!("Entrances {:?} {:?}", entrances.get_start(), entrances.get_end());
     let path = maze.solve_maze( &entrances.get_start(), &entrances.get_end());
-
-    let mut my_path: Vec<Point> = Vec::new();
-    for point in path.into_iter() {
-        my_path.push(**point);
-    }
-
-
+    let mut my_path = path_to_points_system(path);
 
     App::new()
         .insert_resource(MapResource(my_path))
         .insert_resource(MapIndex(0))
+        .insert_resource(WindowDescriptor{
+            title: "Rust Mazer".to_string(),
+            width:598.0,
+            height: 676.0,
+            ..Default::default()
+        })
         .add_plugins(DefaultPlugins)
         .add_plugin(StartupPlugin)
         .run();
 }
+
+
 
 #[derive(Default, Debug)]
 struct MapResource(Vec<Point>);
@@ -55,7 +59,7 @@ struct Actor;
 #[derive(Component)]
 struct Name(String);
 
-fn add_actors(mut commands: Commands) {
+fn add_actors_system(mut commands: Commands) {
     commands.spawn().insert(Actor).insert(Name("Eric Hero".to_string()));
     println!("Greetings from add_actors");
 }
@@ -68,11 +72,6 @@ fn greet_from_actors(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Quer
     }
 }
 
-fn map_info_from_actors(map: Res<MapResource>, map_index: ResMut<MapIndex>,
-    query: Query<&Name, With<Actor>>) {
-        println!("{:?}", map);
-    }
-
 fn print_index(map_index: Res<MapIndex>) {
     println!("{:?}", map_index);
 }
@@ -84,37 +83,58 @@ fn get_path_length(map: Res<MapResource>) {
 pub struct StartupPlugin;
 struct GreetTimer(Timer);
 
-
 impl Plugin for StartupPlugin {
     fn build(&self, app: &mut App) {
         app.insert_resource(GreetTimer(Timer::from_seconds(2.0, true)))
         .add_startup_system(setup_system)
-        .add_startup_system(add_actors)
-        .add_system(map_info_from_actors)
+        .add_startup_system(actor_setup_system)
+        .add_system(move_actor_system)
         .add_system(greet_from_actors);
     }
 }
 
-fn setup_system(mut commands: Commands, asset_server: Res<AssetServer>, mut windows: ResMut<Windows>) {
+fn move_actor_system(map: Res<MapResource>, mut map_index: ResMut<MapIndex>,
+    mut query: Query<(&mut Transform, With<Actor>)>) {
+        println!("{:?}", map.0[map_index.0]);
+        let mut x = map.0[map_index.0].x as f32;
+        let mut y = map.0[map_index.0].y as f32;
+        x = x as f32 * MAP_SCALE as f32 - 200.;
+        y = 200. - y as f32 * MAP_SCALE as f32;
+
+        if map_index.0 < (map.0.len() - 1) {
+            map_index.0 += 1;
+        }
+        else {
+            map_index.0 = 0;
+        }
+
+
+        for (mut transform, actor) in query.iter_mut() {
+            dbg!(x);
+            dbg!(y);
+            if actor {
+                transform.translation = Vec3::new(x ,y ,2.0);
+            }
+        }
+}
+
+fn map_info_from_actors(map: Res<MapResource>, map_index: ResMut<MapIndex>,
+    query: Query<&Name, With<Actor>>) {
+        println!("{:?}", map);
+    }
+
+fn setup_system(mut commands: Commands, asset_server: Res<AssetServer>,mut map: ResMut<MapResource>, mut windows: ResMut<Windows>) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
 
     let window = windows.get_primary_mut().unwrap();
     let (win_w, win_h) = (window.width(), window.height());
     window.set_position(IVec2::new(990, 108));
 
+    let start = map.0[0];
 
-    const ACTOR_SCALE: f32 = 0.5;
-    commands.spawn_bundle(SpriteBundle {
-        texture:  asset_server.load(PLAYER_SPRITE),
-        transform: Transform {
-            scale: Vec3::new(ACTOR_SCALE,ACTOR_SCALE, 1.),
-            ..Default::default()
-        },
-        ..Default::default()
-    });
+    let x = start.x as f32 * MAP_SCALE as f32 - 200.;
+    let y = 200. - start.y as f32 * MAP_SCALE as f32;
 
-
-    const MAP_SCALE: f32 = 10.;
     commands.spawn_bundle(SpriteBundle {
         texture:  asset_server.load(MAP_SPRITE),
         transform: Transform {
@@ -123,4 +143,29 @@ fn setup_system(mut commands: Commands, asset_server: Res<AssetServer>, mut wind
         },
         ..Default::default()
     });
+}
+
+fn actor_setup_system(mut commands: Commands, asset_server: Res<AssetServer>,mut map: ResMut<MapResource>, mut windows: ResMut<Windows>) {
+    let start = map.0[0];
+
+    let x = start.x as f32 * MAP_SCALE as f32 - 200.;
+    let y = 200. - start.y as f32 * MAP_SCALE as f32;
+
+    commands.spawn_bundle(SpriteBundle {
+        texture:  asset_server.load(PLAYER_SPRITE),
+            transform: Transform {
+                scale: Vec3::new(ACTOR_SCALE,ACTOR_SCALE, 1.),
+                translation: Vec3::new(x,y,2.0),
+                ..Default::default()
+            },
+            ..Default::default()})
+        .insert(Actor).insert(Name("Eric Hero".to_string()));
+}
+
+fn path_to_points_system(path: Path) -> Vec<Point> {
+    let mut my_path: Vec<Point> = Vec::new();
+    for point in path.into_iter() {
+        my_path.push(**point);
+    }
+    my_path
 }
